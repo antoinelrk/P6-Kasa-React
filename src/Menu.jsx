475c94663c5079f416d1c715b6d7029e8a5932e5
AntoineLRK <admin@antoinelrk.com>
2023-04-28 09:01:50 +0200

    Starting projet

---
+++ src/Menu.jsx
@@ -0,0 +1,12 @@
+import './Menu.css'
+
+export default function Menu () {
+    return (
+        <nav className='Menu'>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">A Propos</a></li>
+            </ul>
+        </nav>
+    )
+}
