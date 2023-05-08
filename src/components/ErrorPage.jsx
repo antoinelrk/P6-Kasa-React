ece046647b6cb7dfb908a562b17da2e905d8ca2b
AntoineLRK <admin@antoinelrk.com>
2023-05-08 07:39:13 +0200

    Save

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,14 +4,28 @@ function ErrorPage () {
     const error = useRouteError();
 
     return (
-        <div>
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+        <div className="errors-page">
+            <div className="error-container">
+                <h1>{error.status}</h1>
+                <p>{getMessage(error)}</p>                
+            </div>
+                <a href="/" className="return-to-home">Retourner sur la page d'accueil</a>
         </div>
     )
+    
+}
+
+const getMessage = (error) => {
+    let result = ""
+
+    switch (error.status) {
+        case 404:
+            result = `Oups ! La page que vous demandez n'existe pas !`
+            break;
+        default: 
+    }
+
+    return result
 }
 
 export default ErrorPage
