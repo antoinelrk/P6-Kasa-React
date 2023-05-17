1e95912d3dd40d17a387ab8b92001c1791dd7b5a
Antoine LRK <admin@antoinelrk.com>
2023-05-17 11:28:31 +0200

    Clean code and remove unused errors

--- src/pages/Errors.jsx
+++ src/pages/Errors.jsx
@@ -1,15 +1,8 @@
-import { useEffect } from "react";
-import { useParams, useRouteError } from "react-router-dom";
-
 export default function Errors() {
-  const error = useRouteError()
-  useEffect(() => document.title = `La ressource n'a pas été trouvée ! | Erreur 404`)
-
   return (
     <div className="errors-page">
       <div className="error-container">
-        <h1>{error.status}</h1>
-        {/* <p>{error.statusText}</p> */}
+        <h1>404</h1>
         <p>Oups ! La page que vous demandez n'existe pas</p>
       </div>
       <a href="/" className="return-to-home">Retourner sur la page d'accueil</a>
